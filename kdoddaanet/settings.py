4080d006594f70d8087c740e937906bf1fde6dab
David Allouche <david@intuition-software.com>
2025-03-05 12:48:40 +0100

    Unit tests for /account/login and /account/.

--- kdoddaanet/settings.py
+++ kdoddaanet/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = "/account/login"
+LOGIN_REDIRECT_URL = "/account/"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
