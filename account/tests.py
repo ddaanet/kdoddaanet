c53c4f62b56fc21d3ce3bc89a4907ad1a543199e
David Allouche <david@allouche.net>
2025-03-12 09:59:15 +0100

    Remove unwanted comment.

--- account/tests.py
+++ account/tests.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-
 
 class TestLogin(TestCase):
     """Tests for the login view."""
